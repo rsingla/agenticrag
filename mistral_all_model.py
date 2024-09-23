f527597e312dcdd934d19673170a2f260a1924b2
Rajeev Singla <rajeev1.singla@gmail.com>
2024-09-23 03:50:25 -0700

    Updated code for mistral

--- mistral_all_model.py
+++ mistral_all_model.py
@@ -45,5 +45,39 @@ if response.status_code == 200:
 else:
     print(f"Failed to fetch models: {response.status_code} {response.text}")
 
-import json
+
+'''
+This is the list as of 2024-07-29
+
+open-mistral-7b
+mistral-tiny
+mistral-tiny-2312
+open-mistral-nemo
+open-mistral-nemo-2407
+mistral-tiny-2407
+mistral-tiny-latest
+open-mixtral-8x7b
+mistral-small
+mistral-small-2312
+open-mixtral-8x22b
+open-mixtral-8x22b-2404
+mistral-small-2402
+mistral-small-latest
+mistral-small-2409
+mistral-medium-2312
+mistral-medium
+mistral-medium-latest
+mistral-large-2402
+mistral-large-2407
+mistral-large-latest
+codestral-2405
+codestral-latest
+codestral-mamba-2407
+open-codestral-mamba
+codestral-mamba-latest
+pixtral-12b-2409
+pixtral-12b
+pixtral-12b-latest
+mistral-embed
+'''
 
