e5b0a92601a44c709a14794c7bec39a833e215d9
Rajeev Singla <rajeev1.singla@gmail.com>
2024-10-19 16:54:04 -0700

    Added Mistal Fintech and updated mistral helper. Updated the requirements.txt

--- mistral_helper.py
+++ mistral_helper.py
@@ -1,15 +1,21 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
 from mistralai import Mistral
 
-import os
-from dotenv import load_dotenv, find_dotenv    
+def reload_env():
+    load_dotenv()
+
+reload_env()
 
 load_dotenv(find_dotenv()) 
 
-# Set your Mistral API Key as an environment variable.
-# You can find your API key on the Mistral website.
-mistral_api_key = os.getenv("MISTRAL_API_KEY")
 
 def mistral_ai(user_message, model="mistral-large-latest", is_json=False):
+    load_dotenv(find_dotenv()) 
+    # Set your Mistral API Key as an environment variable.
+    # You can find your API key on the Mistral website.
+    mistral_api_key = os.getenv("MISTRAL_API_KEY")
     # Initialize the Mistral client
     client = Mistral(api_key=mistral_api_key)
 
@@ -39,3 +45,29 @@ def mistral_ai(user_message, model="mistral-large-latest", is_json=False):
         )
 
     return chat_response.choices[0].message.content
+
+
+
+def mistral_tiny(user_message):
+
+    api_key = os.environ["MISTRAL_API_KEY"]
+    model = "mistral-tiny"
+
+    client = Mistral(api_key=api_key)
+
+    messages = [
+    {
+        "role": "user",
+        "content": "Who is the most renowned French painter?"
+        }
+    ]
+
+    response = client.chat.complete(
+        model=model,
+        messages=messages,
+        max_tokens=100,
+        temperature=0.5,
+        top_p=0.9
+    )
+
+    return response.choices[0].message.content
